9c787f9371d9246a8a236754370583ad333738c9
Nikita Ablamskiy <shealtov@gmail.com>
2024-08-25 22:07:20 +0300

    Escape sequence fixed

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center px-6 font-bold text-2xl md: max-w-[768px] mx-auto border-x-2 border-x-black dark:border-x-white">
       <div className='flex flex-col gap-2 items-center'>
-        <p>THERE'S NOTHING TO SEE HERE YET.</p>
+        <p className='text-center'>THERE&apos;S NOTHING TO SEE HERE YET.</p>
         <Link className='hover:underline' href="https://t.me/fly0utwest">tg: @fly0utwest</Link>
       </div>
     </main>
