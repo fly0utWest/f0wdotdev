fa6ad325076023b4e2f3c39d41b4dc6cb8be3bd5
fly0utWest <93532590+fly0utWest@users.noreply.github.com>
2024-08-29 23:10:11 +0300

    Big boy commit  (#8)
    * Intermediate commit
    
    * We ball
    
    * typo end lint fixes
    
    * Transition theme added, typo fixed
    
    * tech stack section's ready, contents table dev started
    
    * key index added
    
    * Update globals.css
    
    theme transition deleted
    
    * Biig boy

--- src/shared/nav-side/ui/NavSide.tsx
+++ src/shared/nav-side/ui/NavSide.tsx
@@ -4,18 +4,27 @@ import { ThemeSwitcher } from '@/features/theme-switcher';
 import {
   PiTelegramLogo as TelegramLogo,
   PiGithubLogo as GithubLogo,
+  PiSteamLogo as SteamLogo,
+  PiLastfmLogo as LastFMLogo,
 } from 'react-icons/pi';
 
 export const NavSide = () => {
   return (
-    <nav className="flex flex-row justify-between w-full sticky top-0 min-h-full p-4 border-b-2 border-b-black bg-white p-y-4 dark:bg-black dark:border-b-white md:flex-col md:border-r-2 md:border-r-black md:dark:border-r-white md:w-max md:static md:pb-20 md:justify-normal md:gap-6">
-      <ThemeSwitcher />
-      <div className="flex gap-4 md:self-center md:flex-col">
+    <nav className="flex flex-row items-center justify-between w-full sticky top-0 min-h-full p-4 border-b-2 border-b-black bg-white p-y-4 dark:bg-black dark:border-b-white md:flex-col md:border-r-2 md:border-r-black md:border-b-0 md:dark:border-r-white md:w-max md:relative md:pb-20 md:justify-normal md:gap-6 md:pt-0">
+      <p className="text-sm md:hidden">Socials and stuff -&gt;</p>
+      <div className="flex gap-4 justify-between md:flex-col md:sticky md:top-0 md:pt-4">
+        <ThemeSwitcher />
         <Link href="https://t.me/fly0utwest">
           <TelegramLogo className="w-6 h-6" />
         </Link>
         <Link href="https://github.com/fly0utwest">
           <GithubLogo className="w-6 h-6" />
+        </Link> 
+        <Link href="https://steamcommunity.com/id/fly0utWest/">
+          <SteamLogo className="w-6 h-6" />
+        </Link>
+        <Link href="https://www.last.fm/user/fly0utWest">
+          <LastFMLogo className="w-6 h-6" />
         </Link>
       </div>
     </nav>
--- src/shared/navbar/ui/Navbar.tsx
+++ src/shared/navbar/ui/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className='text-black w-full h-16 border-t-2 bottom-0 px-6 border-t-black fixed flex justify-center bg-white dark:border-t-white dark:bg-black'>
         <div className='w-full md: h-full max-w-[768px] flex items-center justify-center'>
-            <Link className="dark:invert" href={'https://webring.otomir23.me'}>Webring</Link>
+            <Link className="dark:invert text-center" href={''}>Here might be a link to webring someday</Link>
         </div>
     </nav>
   )
