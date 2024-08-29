8d6c30c3b143b9de47e6abb3aa67b5e673e4113b
Nikita Ablamskiy <shealtov@gmail.com>
2024-08-29 13:15:46 +0300

    key index added

--- src/widgets/contents-table/ui/ContentsTable.tsx
+++ src/widgets/contents-table/ui/ContentsTable.tsx
@@ -7,28 +7,31 @@ const ContentsTable = () => {
     description: string;
   }
 
-  const links: ContentsList[] = [{ link: '#info', description: '> ./info.txt' }, {link: "#tech-stack", description: "> ./tech-stack.md"}];
+  const links: ContentsList[] = [
+    { link: '#info', description: '> ./info.txt' },
+    { link: '#tech-stack', description: '> ./tech-stack.md' },
+  ];
 
   return (
     <>
-    <section className="w-full flex flex-col gap-3 mb-5">
-      <h2 className="text-2xl font-bold">
-        <span className="text-violet-400">cd</span> &lt;TAB&gt;
-      </h2>
-      <ul className="flex flex-col gap-4">
-        {links.map((element) => (
-          <li>
-            <Link
-              className="py-2 pr-2 w-max hover:bg-black hover:text-white hover:transition-colors transition-colors dark:hover:invert"
-              href={element.link}
-            >
-              {element.description}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </section>
-    <hr className='border-gray-600 w-full mb-10'/>
+      <section className="w-full flex flex-col gap-3 mb-5">
+        <h2 className="text-2xl font-bold">
+          <span className="text-violet-400">cd</span> &lt;TAB&gt;
+        </h2>
+        <ul className="flex flex-col gap-4">
+          {links.map((element, index) => (
+            <li key={index}>
+              <Link
+                className="py-2 pr-2 w-max hover:bg-black hover:text-white hover:transition-colors transition-colors dark:hover:invert"
+                href={element.link}
+              >
+                {element.description}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <hr className="border-gray-600 w-full mb-10" />
     </>
   );
 };
