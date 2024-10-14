8a870fc9f9b18e6f6884c03e084a80607ebbefbb
Nikita Ablamskiy <shealtov@gmail.com>
2024-10-14 11:44:35 +0300

    stateful underline added dor lang switcher

--- src/features/locale-switcher/ui/LocaleSwitcher.tsx
+++ src/features/locale-switcher/ui/LocaleSwitcher.tsx
@@ -1,16 +1,23 @@
 'use client';
 
+import { useState } from 'react';
 import { i18n, Locale } from '@/shared/config/';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useLang } from '@/shared/config/';
 
 interface LocaleSwitcherProps {
-  dict: string[]
+  dict: string[];
 }
 
-const LocaleSwitcher: React.FC<LocaleSwitcherProps> = ({dict}) => {
+const LocaleSwitcher: React.FC<LocaleSwitcherProps> = ({ dict }) => {
   const pathname = usePathname();
-  const segments = pathname!.split('/').filter(Boolean); // Ensure no empty segments
+  const lang = useLang();
+  const segments = pathname!.split('/').filter(Boolean);
+  const [activeLang, setActiveLang] = useState({
+    en: lang === 'en',
+    ru: lang === 'ru',
+  });
 
   const redirectedPathName = (locale: Locale) => {
     if (!pathname) return '/';
@@ -24,17 +31,20 @@ const LocaleSwitcher: React.FC<LocaleSwitcherProps> = ({dict}) => {
     return `/${segments.join('/')}`;
   };
 
-
   return (
     <div className="text-lg flex flex-row gap-4 justify-center items-center p-2 w-full">
       <Link
-        className="hover:underline text-black dark:text-white"
+        className={`hover:underline text-black dark:text-white${
+          activeLang.en ? ' underline' : ' no-underline'
+        }`}
         href={redirectedPathName('en')}
       >
         {dict[0]}
       </Link>
       <Link
-        className="hover:underline text-black dark:text-white"
+        className={`hover:underline text-black dark:text-white${
+          activeLang.ru ? ' underline' : ' no-underline'
+        }`}
         href={redirectedPathName('ru')}
       >
         {dict[1]}
