3aacc48242f8a0bda9c30cd5c7fb3b118cbd995d
Nikita Ablamskiy <93532590+fly0utWest@users.noreply.github.com>
2024-10-14 14:10:51 +0300

    yet another fix for paths (#31)
    * Deleted template
    
    * SSR for projects and tracks section is added
    
    * Contents table and biosection styles fix for non-js mode
    
    * env var for public api route was added
    
    * non-js version is ready
    
    * public urk for ssr added
    
    * protextion fix
    
    * dynamic fetching for home page
    
    * i18n was added
    
    * stateful underline added dor lang switcher
    
    * regex fix
    
    * fix 2
    
    * neofetch image path fix
    
    * path for error img updated

--- src/widgets/lastfm-widget/ui/ErrorTrackCard.tsx
+++ src/widgets/lastfm-widget/ui/ErrorTrackCard.tsx
@@ -9,7 +9,7 @@ export default async function ErrorTrackCard(): Promise<JSX.Element> {
     <div className="flex flex-row items-center gap-4 min-w-fit min-h-32 text-sm border-2 border-black p-4 dark:border-white text-black dark:text-white">
       <Image
         alt="Music error"
-        src={'/music-error.gif'}
+        src={'/img/music-error.gif'}
         width={60}
         height={60}
       ></Image>
