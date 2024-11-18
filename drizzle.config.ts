5956a6c9b06c2ef90c09122d0263517505e4ea05
Nikita Ablamskiy <shealtov@gmail.com>
2024-11-18 12:29:20 +0300

    pglite pkg added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   out: './drizzle',
   schema: './src/shared/config/db/schema.ts',
   dialect: 'postgresql',
+  driver: 'pglite',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
