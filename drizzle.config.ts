a570133d0a87ce339710daa214faa8c983717099
Nikita Ablamskiy <shealtov@gmail.com>
2024-11-18 23:11:21 +0300

    driver section is deleted in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   out: './drizzle',
   schema: './src/shared/config/db/schema.ts',
   dialect: 'postgresql',
-  driver: 'pglite',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
