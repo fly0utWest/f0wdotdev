38d1cbb73420b60ea63de3943ae02bf64589cd24
Nikita Ablamskiy <93532590+fly0utWest@users.noreply.github.com>
2024-11-21 17:04:08 +0300

    Webring logic is on (#45)
    * Drizzle integration finished
    
    * a little color-related style issue is resolved
    
    * flex wrap for the project tools in project card added
    
    * Webring is on

--- src/shared/ui/ErrorDB.tsx
+++ src/shared/ui/ErrorDB.tsx
@@ -1,13 +1,13 @@
 import React, { ReactNode } from 'react';
 import { MdError as ErrorIcon } from 'react-icons/md';
 
-interface IErrorDBProps {
+interface ErrorDBProps {
   children: ReactNode;
 }
 
 export async function ErrorDB({
   children,
-}: IErrorDBProps): Promise<JSX.Element> {
+}: ErrorDBProps): Promise<JSX.Element> {
   return (
     <div className="flex flex-row gap-4 items-center ">
       <ErrorIcon className="text-red-500" size={24} />
